f9ef274225da8dda4a6e41f333f224cec155c715
Tooba Ahmed <toobs94@gmail.com>
2022-11-23 01:59:40 +0000

    Completed prep-javascript-arrays exercises

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,10 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array: ', languages);
+const firstElement = languages[0];
+console.log('The 1st element of the array is: ', firstElement);
+const thirdElement = languages[2];
+console.log('The 3rd element of the array is: ', thirdElement);
+const length = languages.length;
+console.log('The length of the array is: ', length);
+const lastElement = languages[length - 1];
+console.log('The last element of the array is: ', lastElement);
